02dc5139551a8d97296c36abf4006c1cf41d28d3
julra197 <ralfebert@outlook.com>
2017-11-22 22:09:31 +0100

    check and __len__ implemented

--- Trie.py
+++ Trie.py
@@ -3,31 +3,64 @@
 #Trie data structure for fast storing and retrieving of strings
 #Each string in the data structure can be found in constant time
 
+
 class TrieNode:
     def __init__(self):
         self.isWord = False;
         self.childs = [None] * 27
+        self.counter = 0
         
     def populate(self, dictionaryFile):
+        #process each line, where each line is a unique string for the dictionary
         for line in dictionaryFile:
             newNode = self
+            #all lowercase
             line = line.lower()
+            #populate the trie:
             for char in line:
                 slot = ord(char)-97
-                if slot != -87:
+                if slot >= 0 and slot <=26:
                     if newNode.childs[slot]==None:
                         newNode.childs[slot] = TrieNode()
                     newNode = newNode.childs[slot]
+            #after last character set is word to true
             newNode.isWord = True
+            #check wheter all words are stored:
+            self.counter +=1
     
     def check(self, word):
-        pass
+        currentLevel = self
+        word = word.lower()
+        for char in word:
+            slot = ord(char)-97
+            if(currentLevel.childs[slot] != None):
+                currentLevel = currentLevel.childs[slot]
+            else:
+                return False
+        return currentLevel.isWord
+    
+    #len and count do not properly work
+    def count(self, node):
+        if node.isWord:
+            self.counter += 1
+        for i in range(27):
+            if(node.childs[i] != None):
+                self.count(node.childs[i])
+        return self.counter
+    
+    def __len__(self):
+        self.counter = 0
+        count = self.count(self)
+        self.counter = None
+        return count
+   
+    
 
 
 
-
-root = TrieNode()
-f = open('small', 'r')
-root.populate(f)
-
-print(root.childs[3].childs[14].childs[6].childs[6].childs[4].childs)
+dictionary = TrieNode()
+d = open('large', 'r')
+dictionary.populate(d)
+print(dictionary.check(""))
+print(dictionary.counter)
+print(len(dictionary))
