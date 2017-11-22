bd444087cc2c8a669dcae4244ee7a1ff20f1aaba
julra197 <ralfebert@outlook.com>
2017-11-22 22:20:24 +0100

    len function debuged, populate and check amended

--- Trie.py
+++ Trie.py
@@ -7,8 +7,8 @@
 class TrieNode:
     def __init__(self):
         self.isWord = False;
-        self.childs = [None] * 27
-        self.counter = 0
+        self.childs = [None] * 28
+        self.counter = None
         
     def populate(self, dictionaryFile):
         #process each line, where each line is a unique string for the dictionary
@@ -19,31 +19,32 @@ class TrieNode:
             #populate the trie:
             for char in line:
                 slot = ord(char)-97
-                if slot >= 0 and slot <=26:
+                if slot == -58:
+                    slot = 27
+                if slot >= 0 and slot <=27:
                     if newNode.childs[slot]==None:
                         newNode.childs[slot] = TrieNode()
                     newNode = newNode.childs[slot]
             #after last character set is word to true
             newNode.isWord = True
-            #check wheter all words are stored:
-            self.counter +=1
     
     def check(self, word):
         currentLevel = self
         word = word.lower()
         for char in word:
             slot = ord(char)-97
+            if slot == -58:
+                    slot = 27
             if(currentLevel.childs[slot] != None):
                 currentLevel = currentLevel.childs[slot]
             else:
                 return False
         return currentLevel.isWord
     
-    #len and count do not properly work
     def count(self, node):
         if node.isWord:
             self.counter += 1
-        for i in range(27):
+        for i in range(28):
             if(node.childs[i] != None):
                 self.count(node.childs[i])
         return self.counter
@@ -57,12 +58,12 @@ class TrieNode:
     
 
 
-
 dictionary = TrieNode()
 d = open('large', 'r')
 dictionary.populate(d)
 print(dictionary.check("a"))
 print(dictionary.check("hencoop"))
 print(dictionary.check("zymurgy"))
+print(dictionary.check("aardvark's"))
 print(dictionary.counter)
 print(len(dictionary))
